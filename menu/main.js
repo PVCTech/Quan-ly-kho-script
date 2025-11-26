a34a02d9539646310fa1e01d871e260273d5695d
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2025-11-26 13:50:51 +0700

    Create main.js

---
+++ menu/main.js
@@ -0,0 +1,42 @@
+<table style="width:100%;box-sizing:border-box;background-color:#0e8d44;color:white;padding:8px;text-align:center;">
+      <tr>
+        <td>
+          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec">
+            <div class="menu__button">
+              Trang chủ
+            </div>
+          </a>
+        </td>
+
+        <td>
+          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=danh-muc-vat-tu">
+            <div class="menu__button">
+              Danh mục vật tư
+            </div>
+          </a>
+        </td>
+
+        <td>
+          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=tra-cuu-vat-tu">
+          <div class="menu__button">
+            Tìm, Xuất vật tư
+          </div>
+          </a>
+        </td>
+     
+        <td>
+          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=nhap-kho">
+            <div class="menu__button">
+              Nhập kho
+            </div>
+          </a>
+        </td>
+        <td>
+          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=xuat-kho">
+            <div class="menu__button">
+              Xuất theo kệ
+            </div>
+          </a>
+        </td>
+      </tr>
+    </table>
