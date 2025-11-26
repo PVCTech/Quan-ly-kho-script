09051c037d34ebd990ef18ce84889907ba39ef27
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2025-11-26 13:46:05 +0700

    Create main.js

---
+++ vatTu/main.js
@@ -0,0 +1,11 @@
+class VatTu
+{
+  constructor(ma, ten = '', dacDiem ='', ghiChu = '', soLuongViTri = [])
+  {
+    this.ma = ma;
+    this.ten =ten;
+    this.dacDiem = dacDiem;
+    this.ghiChu = ghiChu;
+    this.soLuongViTri = soLuongViTri;
+  }
+}
