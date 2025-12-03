0bb5115c66307cd648166fcf4a84c218a2103063
phamcuong8868 <phamcuong8868@gmail.com>
2025-12-03 21:15:26 +0700

    edit menu mainjs

--- menu/main.js
+++ menu/main.js
@@ -52,5 +52,43 @@ menuTop.innerHTML = `
                   </td>
             </tr>
       </table>
+      <div class="menu__left" id="menuLeft">
+            <div>
+                  <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec" class="menu__button">
+                        Trang chủ
+                  </a>
+            </div>
+
+            <div>
+                  <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=nhap-kho" class="menu__button">
+                        Nhập kho
+                  </a>
+            </div>
+
+            <div>
+                  <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=danh-muc-vat-tu" class="menu__button">
+                        Danh mục vật tư
+                  </a>
+            </div>
+            
+            <div>
+                  <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=tra-cuu-vat-tu" class="menu__button">
+                        Tìm, Xuất vật tư
+                  </a>
+            </div>
+      </div>
 `;      
 document.body.insertBefore(menuTop, document.body.firstChild);
+
+function menuLeft_toggle()
+{
+      let menuLeft = document.getElementById("menuLeft");
+      if (menuLeft.style.left == "0px")
+      {
+            menuLeft.style.display = "none";
+      }
+      else
+      {
+            menuLeft.style.display = "block";
+      }
+}
