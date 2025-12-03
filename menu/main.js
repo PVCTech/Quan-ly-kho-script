c48cc0d926b22fc77fd5a0b5b5f2ea256409c2ae
phamcuong8868 <phamcuong8868@gmail.com>
2025-12-03 20:52:13 +0700

    edit menu mainjs

--- menu/main.js
+++ menu/main.js
@@ -1,56 +1,56 @@
-class MenuTop
-{
-
-      render()
-      {
-            let menu_table = document.createElement('table');
-                  let menu_tr = document.createElement('tr');
-                  menu_tr.innerHTML = `
-                        <td style="width:50px;">
-                              <a href="#">
-                                    <img src="https://pvctech.github.io/Quan-ly-kho-script/menu/img/menu.svg" style="height:30px;" class="menu__icon-menu">
-                              </a>
-                        </td>
-                        
-                        <td>
-                              <div style="display:flex; gap:10px;" id="menu_mainButton_top">
-                                    <div class="menu__button">
-                                          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec">
-                                                Trang chủ
-                                          </a>
-                                    </div>
-
-                                    <div class="menu__button">
-                                          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=nhap-kho">
-                                                Nhập kho
-                                          </a>
-                                    </div>
-
-                                    <div class="menu__button">
-                                          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=danh-muc-vat-tu">
-                                                Danh mục vật tư
-                                          </a>
-                                    </div>
-                                    
-                                    <div class="menu__button">
-                                          <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=tra-cuu-vat-tu">
-                                                Tìm, Xuất vật tư
-                                          </a>
-                                    </div>
+let menuTop = document.createElement('div');
+menuTop.id = "menuTop";
+menuTop.className = "menu__background";
+      menuTop.style.position = "fixed";
+      menuTop.style.top = "0";
+      menuTop.style.left = "0";
+      menuTop.style.width = "100%";
+      menuTop.style.height = "50px";
+      menuTop.style.zIndex = "1000";
+document.body.appendChild(menuTop);
+menuTop.innerHTML = `
+      <table style="width:100%; height:100%;">
+            <tr>
+                  <td style="width:50px;">
+                        <a href="#">
+                              <img src="https://pvctech.github.io/Quan-ly-kho-script/menu/img/menu.svg" style="height:30px;" class="menu__icon-menu">
+                        </a>
+                  </td>
+                  
+                  <td>
+                        <div style="display:flex; gap:10px;" id="menu_mainButton_top" class="menu__mainButton_top">
+                              <div class="menu__button">
+                                    <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec">
+                                          Trang chủ
+                                    </a>
                               </div>
-                        </td>
 
-                        <td style="width:50px;">
-                              <a href="#">
-                                    <img src="https://pvctech.github.io/Quan-ly-kho-script/menu/img/user.svg" style="height:30px;" class="menu__icon-menu">
-                              </a>
-                        </td>
-                        `;
-            menu_table.appendChild(menu_tr);
-            menu_table.className = "menu__background";
-            document.body.insertBefore(menu_table, document.body.firstChild);
-      }
-}
+                              <div class="menu__button">
+                                    <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=nhap-kho">
+                                          Nhập kho
+                                    </a>
+                              </div>
 
-const menuTop = new MenuTop();
-menuTop.render();
+                              <div class="menu__button">
+                                    <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=danh-muc-vat-tu">
+                                          Danh mục vật tư
+                                    </a>
+                              </div>
+                              
+                              <div class="menu__button">
+                                    <a href="https://script.google.com/macros/s/AKfycbxDnHxsFqRivAKjnJWYGzCq8OB819sRS9vT9-X9K1B56zfqsVk8TvJf8_NnX3JXYIz6/exec?page=tra-cuu-vat-tu">
+                                          Tìm, Xuất vật tư
+                                    </a>
+                              </div>
+                        </div>
+                  </td>
+
+                  <td style="width:50px;">
+                        <a href="#">
+                              <img src="https://pvctech.github.io/Quan-ly-kho-script/menu/img/user.svg" style="height:30px;" class="menu__icon-menu">
+                        </a>
+                  </td>
+            </tr>
+      </table>
+`;      
+document.body.insertBefore(menuTop, document.body.firstChild);
